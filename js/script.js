4baa2a745ddb202ecb51bf3fa74261054f7556b7
root <kdquon@ucdavis.edu>
2017-11-20 18:14:31 -0800

    Homework

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    Typed.new('#typed', {
+        strings: ["sucks", "is weird", "is creepy", "farts"],
+        typeSpeed: 75,
+        backDelay: 900,
+        loop: true
+    });
+});
